50b88c3d7f718d5c76407ee77f375fff5a20fde4
hikaru0506 <79984243+hikaru0506@users.noreply.github.com>
2021-07-09 20:00:36 +0900

    hello

--- scrape.py
+++ scrape.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib import request
-
 url = 'https://www.aozora.gr.jp/cards/001475/files/53453_50584.html'
 response = request.urlopen(url)
 soup = BeautifulSoup(response)
 response.close()
-
 main_text = soup.find('div', class_='main_text')
 
 f = open('main_text.xml', 'w')
