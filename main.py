4aa14ad1d3728072e16cbfa76e4fffa9d03dc40a
Noah Belza SAKANE <58982559+b4e4l4z4a@users.noreply.github.com>
2021-07-09 21:22:35 +0900

    図書カードから作品のURLを抽出したい

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS4
 from urllib import request
 
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
 
 
 def main() -> None:
-    url: str = "https://www.aozora.gr.jp/cards/001475/files/53453_50584.html"
-    scrape(url)
+    #url: str = "https://www.aozora.gr.jp/cards/001475/files/53453_50584.html"
+    library_card_url: str = "https://www.aozora.gr.jp/cards/000879/card4872.html"
+    scrape_library_card(library_card_url)
 
 
-def scrape(url: str) -> None:
+def scrape_library_card(url: str) -> None:
     response = request.urlopen(url)
-    soup = BeautifulSoup(response)
+    soup = BS4(response, 'html.parser')
     response.close()
-    main_text = soup.find('div', class_='main_text')
-    f = open('main_text.xml', 'w')
-    f.write(main_text.__str__())
-    f.close()
+
+    buffer = [
+        element for div in soup('div', align='right')
+        for element in div('a')
+    ]
+
+    for element in buffer:
+        print(element)
+        print(element('href'))
+
+    print(url)
+
+    # def scrape(url: str) -> None:
+    #    response = request.urlopen(url)
+    #    soup = BeautifulSoup(response)
+    #    response.close()
+    #    main_text = soup.find('div', class_='main_text')
+    #    f = open('main_text.xml', 'w')
+    #    f.write(main_text.__str__())
+    #    f.close()
+
+
+if __name__ == "__main__":
+    main()
